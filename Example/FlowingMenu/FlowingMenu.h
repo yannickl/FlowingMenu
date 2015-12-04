742c7e6106b637ee23efb3159eec3502ad5c45cb
Yannick Loriot <yannick.loriot@gmail.com>
2015-12-04 12:54:24 +0100

    [ADD] Carthage supports

---
+++ Example/FlowingMenu/FlowingMenu.h
@@ -0,0 +1,19 @@
+//
+//  FlowingMenu.h
+//  FlowingMenu
+//
+//  Created by Yannick LORIOT on 04/12/15.
+//  Copyright © 2015 Yannick LORIOT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlowingMenu.
+FOUNDATION_EXPORT double FlowingMenuVersionNumber;
+
+//! Project version string for FlowingMenu.
+FOUNDATION_EXPORT const unsigned char FlowingMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlowingMenu/PublicHeader.h>
+
+
